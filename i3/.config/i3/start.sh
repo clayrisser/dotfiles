8b66601e67931e9f6b79c0fe80560c96cce94911
Jam Risser <jam@codejam.ninja>
2019-11-25 07:27:16 -0600

    Updated background/.background.jpg

--- i3/.config/i3/start.sh
+++ i3/.config/i3/start.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# xset dpms 610
-# xautolock -time 10 -locker 'i3lock-next "Input Password" "Hurmit Nerd Font Mono" 24'
-# exec oneko &
-# fluxgui &
-
-# xmodmap ~/.Xmodmap
 bash ~/.active_screenlayout
 sleep 1
-feh --bg-scale ~/.background.jpg
+
+# START SERVICES
 # compton -f --config ~/.config/compton.conf &
-redshift -l 30.267153:-97.743057 &
-polybar main &
+# xset dpms 610
+# xautolock -time 10 -locker 'i3lock-next "Input Password" "Hurmit Nerd Font Mono" 24'
+# fluxgui &
+# xmodmap ~/.Xmodmap
+feh --bg-scale ~/.background.jpg &
 browserpass &
+redshift -l 30.267153:-97.743057 &
+i3-msg restart &
+
+# START PROGRAMS
+# exec oneko &
 sleep 1
-nm-applet &
-cbatticon &
-blueman-applet &
-pulseaudio --start &
+polybar main &
 /opt/piavpn/bin/pia-client &
+blueman-applet &
+cbatticon &
+nm-applet &
+pulseaudio --start &
 terminator &
-sleep 1
 terminator &
